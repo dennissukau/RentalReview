8f66858ae0bf79605316434cc3288a0f567d8c5d
dennissukau <dennis.sukau@students.mq.edu.au>
2024-09-04 01:29:10 +1000

    Start

---
+++ src/main/java/com/rentalreview/RentalReviewApplication.java
@@ -0,0 +1,13 @@
+package com.rentalreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentalReviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rentalreview/RentalReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rentalreview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalReviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
